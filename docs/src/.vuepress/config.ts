4aa42eaa40a68a43fe9373d5c8b60bbc9cd187ce
Mykola Hohsdze <koliagogsadze@gmail.com>
2021-07-08 01:50:16 +0300

    add test docs

---
+++ docs/src/.vuepress/config.ts
@@ -0,0 +1,16 @@
+import { defineUserConfig } from 'vuepress'
+import type { DefaultThemeOptions } from 'vuepress'
+
+export default defineUserConfig<DefaultThemeOptions>({
+    themeConfig: {
+        logo: 'https://vuejs.org/images/logo.png',
+        navbar: [
+            { text: 'Home', link: '/README.md' },
+            { text: 'Guide', link: '/guide/getting_started.md' },
+            { text: 'Contributing', link: '/CONTRIBUTING.md' },
+        ],
+    },
+    lang: 'en-US',
+    title: 'cpu features',
+    description: 'A cross platform C99 library to get cpu features at runtime',
+})
